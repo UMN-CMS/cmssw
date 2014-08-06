ec59054018a3c551a8e8263128a80408868f551e
Andrew Evans <aevans@physics.umn.edu>
2014-08-06 13:45:19 -0500

    Updated the new digi making files to work with MC and data/

--- CalibCalorimetry/CaloTPG/tpDigisFromRawMC_cfg.py
+++ CalibCalorimetry/CaloTPG/tpDigisFromRawMC_cfg.py
@@ -1,6 +1,6 @@
-mport FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Config as cms
  
-process = cms.Process("HFCALIB")
+process = cms.Process("MCDIGIS")
 
 
 #--- Steps available to this configuration ---#
@@ -58,7 +58,7 @@ if(isData):
       TempFN=['file:/hdfs/cms/user/lesko/MinBiasWithDigiAndReco/MinBias2012CDigiAndReco_041.root']
       
 else:
-    TempFN=['file:/hdfs/cms/phedex/store/relval/CMSSW_7_0_0_pre11/RelValZEE_13/GEN-SIM-DIGI-RAW-HLTDEBUG/PU50ns_POSTLS162_V5_OldTrk-v1/00000/48CC1D33-6E6A-E311-8855-001D09F251EF.root','file:/hdfs/cms/phedex/store/relval/CMSSW_7_0_0_pre11/RelValZEE_13/GEN-SIM-DIGI-RAW-HLTDEBUG/PU50ns_POSTLS162_V5_OldTrk-v1/00000/5668ACA2-6B6A-E311-A71E-02163E00E76C.root','file:/hdfs/cms/phedex/store/relval/CMSSW_7_0_0_pre11/RelValZEE_13/GEN-SIM-DIGI-RAW-HLTDEBUG/PU50ns_POSTLS162_V5_OldTrk-v1/00000/5E0D6BBD-6C6A-E311-9202-0025904B2FD8.root','file:/hdfs/cms/phedex/store/relval/CMSSW_7_0_0_pre11/RelValZEE_13/GEN-SIM-DIGI-RAW-HLTDEBUG/PU50ns_POSTLS162_V5_OldTrk-v1/00000/608D9D23-6B6A-E311-AEC2-E0CB4E55365D.root']
+    TempFN=['file:/data/whybee1a/user/aevans/digis/VBFMCDIGIs06-6C.root']
 
 
 
@@ -70,11 +70,11 @@ fileNames = cms.untracked.vstring(TempFN )
     
 )
 
-#process.out = cms.OutputModule( "PoolOutputModule",
-# fileName = cms.untracked.string("lumprod.root"),
-# SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
-# outputCommands = cms.untracked.vstring( 'keep *' )
-#)
+process.out = cms.OutputModule( "PoolOutputModule",
+    fileName = cms.untracked.string("MCDIGIoutput.root"),
+    SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+    outputCommands = cms.untracked.vstring( 'keep *_simHcalTriggerPrimitiveDigis_*_*' )
+)
 
 ###--- (3) Re-RECO from RAW ---###
 ### Auto generated configuration file using Revision: 1.381.2.6
@@ -175,7 +175,7 @@ process.calibPreSequence = cms.Sequence(process.boolTrue)
 #process.digitisation_step = cms.Path(process.pdigi)
 #process.L1simulation_step = cms.Path(process.SimL1Emulator)
 #process.digi2raw_step = cms.Path(process.DigiToRaw)
-#process.endjob_step = cms.EndPath(process.out)
+process.endjob_step = cms.EndPath(process.out)
 #process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
 #process.raw2digi_step = cms.Path(process.RawToDigi)
 # Schedule definition
@@ -191,6 +191,8 @@ if( isData):
 #process.reconstructionFromRawSequence+
     #process.p = cms.Path(process.reconstructionFromRawSequence+ process.calibPreSequence + process.simHcalTriggerPrimitiveDigis + process.DataAna+process.FullGraph)
 else:
-    process.p = cms.Path( process.calibPreSequence + process.simHcalTriggerPrimitiveDigis + process.test + process.CutAs)
-#process.schedule = cms.Schedule(process.p,process.endjob_step)
+    process.p = cms.Path( process.calibPreSequence + process.simHcalTriggerPrimitiveDigis
+                       # + process.test + process.CutAs
+                        )
+    process.schedule = cms.Schedule(process.p,process.endjob_step)
 
