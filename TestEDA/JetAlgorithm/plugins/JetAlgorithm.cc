a5bbaa774fa8ac5b1c970d8315d2f0519063acda
Andrew Evans <aevans@physics.umn.edu>
2014-08-20 15:15:26 -0500

    Updated jet algorithm to include a struct for each jet and check that the seed cell is bigger than the periphery

--- TestEDA/JetAlgorithm/plugins/JetAlgorithm.cc
+++ TestEDA/JetAlgorithm/plugins/JetAlgorithm.cc
@@ -1,8 +1,6 @@
 // -*- C++ -*-
-//
-// Package:    TestEDA/JetAlgorithm
 // Class:      JetAlgorithm
-// 
+//
 /**\class JetAlgorithm JetAlgorithm.cc TestEDA/DataAnalizer/src/JetAlgorithm.cc
 
  Description: [one line class summary]
@@ -30,7 +28,6 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-
 
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
@@ -62,6 +59,10 @@
 #include <iostream>
 #include <vector>
 
+//GenJets
+#include "DataFormats/JetReco/interface/GenJet.h"
+
+
 using namespace std;
 
 
@@ -78,6 +79,15 @@ public:
     explicit JetAlgorithm(const edm::ParameterSet&);
     ~JetAlgorithm();
 
+    struct Jet
+    {
+        float seedEt;     //Energy of center cell
+        float jetEt;      //Energy of 3x3 area centerd on cell
+        bool pass;        //true means its a good jet seed cell false means its not
+
+
+
+    };
 
 
 private:
@@ -115,14 +125,14 @@ private:
 JetAlgorithm::JetAlgorithm(const edm::ParameterSet& iConfig)
 {
     edm::Service<TFileService> fs;
-//Generic Constructor For TH2D: (const char* name, const char* title, Int_t nbinsx, Double_t xlow, Double_t xup, Int_t nbinsy, Double_t ylow, Double_t yup) I'm choosing x to be ieta and y to iphi 
-    HFEtPos = fs->make<TH2D>("HFEtPos", "Forward HF Et", 40, 0.0, 40, 72, 0.0, 72);
-    HFEtNeg = fs->make<TH2D>("HFEtNeg", "Backward HF Et", 40, 0.0, 40, 72, 0.0, 72);
-    HFEt = fs->make<TH1D>("HFEt", "Et in HF", 100, 0.0, 100);
-    HFPosJets =fs->make<TH1D>("HFPosJets", "Positive HF 3x3 Jet Et", 100, 0.0, 100);
-    HFNegJets = fs->make<TH1D>("HFNegJets", "Negative HF 3x3 Jet Et", 100, 0.0, 100);
-    HFNegRatio = fs->make<TH1D>("HFNegRatio", "Neg HF 3x3 vs Seed Ratio", 50, 0.0, 2);
-    HFPosRatio = fs->make<TH1D>("HFPosRatio", "Pos HF 3x3 vs Seed Ratio", 50, 0.0, 2);
+//Generic Constructor For TH2D: (const char* name, const char* title, Int_t nbinsx, Double_t xlow, Double_t xup, Int_t nbinsy, Double_t ylow, Double_t yup) I'm choosing x to be ieta and y to iphi
+    HFEtPos = fs->make<TH2D>("HFEtPos", "Forward HF Et", 10, 30, 40, 72, 0.0, 72);
+    HFEtNeg = fs->make<TH2D>("HFEtNeg", "Backward HF Et", 10, 30, 40, 72, 0.0, 72);
+    HFEt = fs->make<TH1D>("HFEt", "Et in HF", 100, 0.0, 300);
+    HFPosJets =fs->make<TH1D>("HFPosJets", "Positive HF 3x3 Jet Et", 100, 0.0, 300);
+    HFNegJets = fs->make<TH1D>("HFNegJets", "Negative HF 3x3 Jet Et", 100, 0.0, 300);
+    HFNegRatio = fs->make<TH1D>("HFNegRatio", "Neg HF 3x3 vs Seed Ratio", 50, 0.0, 1.1);
+    HFPosRatio = fs->make<TH1D>("HFPosRatio", "Pos HF 3x3 vs Seed Ratio", 50, 0.0, 1.1);
 }
 JetAlgorithm::~JetAlgorithm() {
 
@@ -142,6 +152,7 @@ JetAlgorithm::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
     //     cout << "beginning";
     using namespace edm;
+    using namespace reco;
 
     //     cout << "test \n";              //this cout statement does display
 
@@ -150,6 +161,21 @@ JetAlgorithm::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     double PosJets [40][72];
     double NegJets [40][72];
     bool nonZero = false;
+    bool isPosJet = false;
+    bool isNegJet = false;
+    bool isData = iEvent.isRealData();
+    Jet myJet = Jet();
+    myJet.pass = false;
+    Jet HFarrayPos [40][72];
+    Jet HFarrayNeg [40][72];
+
+    cout << "is data: ";
+    cout << isData << endl;
+
+    Handle<std::vector<reco::GenJet>> pIn;
+    iEvent.getByLabel("ak5GenJets", pIn); //gets truth jets
+    edm::Service<TFileService> fs;
+
 
     edm::ESHandle<CaloTPGTranscoder> outTranscoder;
     iSetup.get<CaloTPGRecord>().get(outTranscoder);
@@ -159,104 +185,159 @@ JetAlgorithm::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     // Also, I need to convert from compressed Et to real Et: I think this is done by the outTranscoder.  This sequence may need modification pending talking to Zach
     edm::Handle<HcalTrigPrimDigiCollection> hfpr_digi;
     iEvent.getByLabel("simHcalTriggerPrimitiveDigis", hfpr_digi);
+
+    for(unsigned int k = 0; k < pIn->size(); ++k)  //checks to see if any truth jets are in HF
+    {
+  //      cout << "Eta Values: ";
+  //      cout << pIn->at(k).eta() << endl;
+        if(pIn->at(k).eta() > 2.964 && pIn->at(k).eta() < 4.716)
+        {
+            isPosJet = true;
+            cout << "PosJet with Eta: ";
+            cout << pIn->at(k).eta() << endl;
+        }
+        else if(pIn->at(k).eta() < -2.964 && pIn->at(k).eta() > -4.716)
+        {
+            isNegJet = true;
+            cout << "NegJet with Eta: ";
+            cout << pIn->at(k).eta() << endl;
+        }
+
+    }
+    if(isPosJet)
+    {
+        HFEtPos = fs->make<TH2D>("HFEtPos", "Forward HF Et", 10, 30, 40, 72, 0.0, 72);
+        cout << "Writing new Histograms" << endl;
+    }
+    if(isNegJet)
+    {
+        HFEtNeg = fs->make<TH2D>("HFEtNeg", "Backward HF Et", 10, 30, 40, 72, 0.0, 72);
+        cout << "Writing new Histograms" << endl;
+    }
+
     //     cout << "Just before loop \n";
     if(hfpr_digi->begin()==hfpr_digi->end())
     {
-        cout << "empty";
+        cout << "empty" << endl;
     }
 
     // Make Histograms
-    edm::Service<TFileService> fs;
-    // TODO-ANDREW: You will probably want to rename these so they don't all have the same name!
 
-    for(HcalTrigPrimDigiCollection::const_iterator tp = hfpr_digi->begin(); tp != hfpr_digi->end(); ++tp)
+
+    if(isPosJet || isNegJet) //checks that there is an HF jet
     {
-        //         cout << "HI";                    //this cout statement does not display
-        if(abs(tp->id().ieta()) < 30 || abs(tp->id().ieta()) > 39)
+        for(HcalTrigPrimDigiCollection::const_iterator tp = hfpr_digi->begin(); tp != hfpr_digi->end(); ++tp)
         {
-            //             cout << "useful part of HF \n";
-            continue;    //Grabs the useful part of HF
-        }
-        //We need the sample of interest (SOI)
 
-        HFEt->Fill(outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
+            if(abs(tp->id().ieta()) < 30 || abs(tp->id().ieta()) > 39)  //Doesn't use the non useful part of HF or the other parts of the detector
+            {
 
-        if(tp->id().ieta() < 0)
-        {
-            EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
-            EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()+1] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
-            HFEtNeg->SetBinContent(abs(tp->id().ieta()),tp->id().iphi(), outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
-            HFEtNeg->SetBinContent(abs(tp->id().ieta()),tp->id().iphi()+1, outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
-        }
-        else
-        {
-            EtArrayPos [tp->id().ieta()][tp->id().iphi()] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
-            EtArrayPos [tp->id().ieta()][tp->id().iphi()+1] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
-            HFEtPos->SetBinContent(tp->id().ieta(),tp->id().iphi(), outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
-            HFEtPos->SetBinContent(tp->id().ieta(),tp->id().iphi()+1, outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
-        }
+                continue;
+            }
+
+            //We need the sample of interest (SOI)
+            //Fills out the Et distribution
+            HFEt->Fill(outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
+
+            if(tp->id().ieta() < 0 && isNegJet)
+            {
+                EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
+                EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()+1] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
+                HFEtNeg->SetBinContent(abs(tp->id().ieta())-30,tp->id().iphi(), outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
+                HFEtNeg->SetBinContent(abs(tp->id().ieta())-30,tp->id().iphi()+1, outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
+                if(outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()) > .01)
+                {
+                    cout << "Setting Bin Content for HFEtNeg" << endl;
+                    cout << outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()) << endl;
+                }
+            }
+            else if (tp->id().ieta() > 0 && isPosJet)
+            {
+                EtArrayPos [tp->id().ieta()][tp->id().iphi()] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
+                EtArrayPos [tp->id().ieta()][tp->id().iphi()+1] = outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt());
+                HFEtPos->SetBinContent(tp->id().ieta()-30,tp->id().iphi(), outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
+                HFEtPos->SetBinContent(tp->id().ieta()-30,tp->id().iphi()+1, outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()));
+                if(outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()) > .01)
+                {
+                    cout << "Setting Bin Content for HFEtPos" << endl;
+                    cout << outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()) << endl;
+                }
+            }
     /*    cout << tp->id().ieta() << " and " << tp->id().iphi() <<endl;
         cout << EtArrayPos [tp->id().ieta()][tp->id().iphi()] <<endl;
         cout << EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()] << endl << endl; */
 
-        if(EtArrayPos [tp->id().ieta()][tp->id().iphi()] + EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()] < 0) //to make compiler happy
-        {
-            cout << "uh oh! negative number! :(" << endl;
-        }
+            if(EtArrayPos [tp->id().ieta()][tp->id().iphi()] + EtArrayNeg [abs(tp->id().ieta())][tp->id().iphi()] < 0) //to make compiler happy
+            {
+                cout << "uh oh! negative number! :(" << endl;
+            }
 
-        if(outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()) > .01)
-        {
-            nonZero = true;
+            if(outTranscoder->hcaletValue(tp->id(), tp->SOI_compressedEt()) > .01)
+            {
+                nonZero = true;
+                cout << nonZero << endl;
+            }
         }
     }
     //     cout << "\nJust after loop \n";
-    if(nonZero)
-    {
-        HFEtNeg = fs->make<TH2D>("HFEtNeg", "Backward HF Et", 40, 0.0, 40, 72, 0.0, 72);
-        HFEtPos = fs->make<TH2D>("HFEtPos", "Forward HF Et", 40, 0.0, 40, 72, 0.0, 72);
-    }
 
-    //Now that the DIGIs are read in and graphed, this part of the code actually tries to find jets.  It makes a 3x6 ietaxiphi sliding window of jets
+
+    //Now that the DIGIs are read in and graphed, this part of the code actually tries to find jets.
     if(true)
-    {    
-        
+    {
+
         for(int i = 31; i < 39; i++) //loops over ieta
         {
             for(int j = 3; j < 70; j+=2) //loops over iphi
             {
-                if(EtArrayNeg [i][j] > 10)  //10GeV seed cell threshhold
+                if(EtArrayNeg [i][j] > 10 && isNegJet)  //10GeV seed cell threshhold
                 {
-                NegJets [i][j] = EtArrayNeg [i-1][j-2] + EtArrayNeg [i-1][j] + EtArrayNeg [i-1][j+2] + 
-                                 EtArrayNeg [i][j-2]   + EtArrayNeg [i][j]   + EtArrayNeg [i][j+2]   +
-                                 EtArrayNeg [i+1][j-2] + EtArrayNeg [i+1][j] + EtArrayNeg [i+1][j+2];
-                
-                HFNegJets->Fill(NegJets [i][j]);
+                    NegJets [i][j] = EtArrayNeg [i-1][j-2] + EtArrayNeg [i-1][j] + EtArrayNeg [i-1][j+2] +
+                                     EtArrayNeg [i][j-2]   + EtArrayNeg [i][j]   + EtArrayNeg [i][j+2]   +
+                                     EtArrayNeg [i+1][j-2] + EtArrayNeg [i+1][j] + EtArrayNeg [i+1][j+2];
 
-                HFNegRatio->Fill(EtArrayNeg [i][j]/NegJets [i][j]);
+                    //checks that the seed is greater than the periphery
+                    if(EtArrayNeg [i][j] > EtArrayNeg [i-1][j-2] && EtArrayNeg [i][j] > EtArrayNeg [i-1][j]   && EtArrayNeg [i][j] > EtArrayNeg [i-1][j+2] &&
+                       EtArrayNeg [i][j] > EtArrayNeg [i][j-2]                                                && EtArrayNeg [i][j] > EtArrayNeg [i][j+2]   &&
+                       EtArrayNeg [i][j] > EtArrayNeg [i+1][j-2] && EtArrayNeg [i][j] > EtArrayNeg [i+1][j]   && EtArrayNeg [i][j] > EtArrayNeg [i+1][j+2])
+                    {
+                        HFarrayNeg[i][j].jetEt = NegJets [i][j];
+                        HFarrayNeg[i][j].seedEt = EtArrayNeg [i][j];
+                        HFarrayNeg[i][j].pass = true;                       //initial true setting for whether it is a good jet
+
+                    }
+                    HFNegJets->Fill(NegJets [i][j]);
+                    HFNegRatio->Fill(EtArrayNeg [i][j]/NegJets [i][j]);
                 }
-                if(EtArrayPos [i][j] > 10)   //10GeV seed cell threshhold
+                if(EtArrayPos [i][j] > 10 && isPosJet)   //10GeV seed cell threshhold
                 {
-                PosJets [i][j] = EtArrayPos [i-1][j-2] + EtArrayPos [i-1][j] + EtArrayPos [i-1][j+2] +
-                                 EtArrayPos [i][j-2]   + EtArrayPos [i][j]   + EtArrayPos [i][j+2]   +
-                                 EtArrayPos [i+1][j-2] + EtArrayPos [i+1][j] + EtArrayPos [i+1][j+2];
+                    PosJets [i][j] = EtArrayPos [i-1][j-2] + EtArrayPos [i-1][j] + EtArrayPos [i-1][j+2] +
+                                     EtArrayPos [i][j-2]   + EtArrayPos [i][j]   + EtArrayPos [i][j+2]   +
+                                     EtArrayPos [i+1][j-2] + EtArrayPos [i+1][j] + EtArrayPos [i+1][j+2];
 
-                HFPosJets->Fill(PosJets [i][j]);
+                    //checks that the seed is greater than the periphery
+                    if(EtArrayPos [i][j] > EtArrayPos [i-1][j-2] && EtArrayPos [i][j] > EtArrayPos [i-1][j]   && EtArrayPos [i][j] > EtArrayPos [i-1][j+2] &&
+                       EtArrayNeg [i][j] > EtArrayPos [i][j-2]                                                && EtArrayNeg [i][j] > EtArrayPos [i][j+2]   &&
+                       EtArrayNeg [i][j] > EtArrayPos [i+1][j-2] && EtArrayPos [i][j] > EtArrayPos [i+1][j]   && EtArrayNeg [i][j] > EtArrayPos [i+1][j+2])
+                    {
+                        HFarrayPos[i][j].jetEt = PosJets [i][j];
+                        HFarrayPos[i][j].seedEt = EtArrayPos [i][j];
+                        HFarrayPos[i][j].pass = true;                       //initial true setting for whether it is a good jet
 
-                HFPosRatio->Fill(EtArrayPos [i][j]/PosJets [i][j]);
+                    }
+                    HFPosJets->Fill(PosJets [i][j]);
+
+                    HFPosRatio->Fill(EtArrayPos [i][j]/PosJets [i][j]);
                 }
             }
         }
     }
 
-    
 
-    if(PosJets [1][1] + NegJets [1][1] < 0)   //random usage of arrays to make compiler happy (designed to never print)
-    {
-        cout << "Happy Birthday!" << endl;
-    }
-                                
-    
-    
+
+
+
+
 
 
 
