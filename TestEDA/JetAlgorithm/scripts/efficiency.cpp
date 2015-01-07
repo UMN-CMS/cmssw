c738e0c770ee6788ea65850c833c979298226a34
Andrew Evans <aevans@physics.umn.edu>
2015-01-07 16:16:58 -0600

    Added scripts folder for realz this time

---
+++ TestEDA/JetAlgorithm/scripts/efficiency.cpp
@@ -0,0 +1,55 @@
+//Standard Library
+#include <string>
+#include <iostream>
+
+//Root Includes
+#include <TH1D.h>
+#include <TFile.h>
+#include <TGraphAsymmErrors.h>
+
+int efficiency()
+{
+    std::string inFile = "/home/aevans/CMS/CMSSW_7_1_0_pre5/src/TestEDA/JetAlgorithm/plugins/jets.root";
+    std::string outFile = "eff.root";
+
+    TFile* inTfile = new TFile(inFile.c_str());
+
+    const int numberOfHists = 6;
+    TH1D* histograms[numberOfHists];
+    inTfile->GetObject("/jetFinder/EtMatched", histograms[0]);
+    inTfile->GetObject("/jetFinder/Et", histograms[1]);
+    inTfile->GetObject("/jetFinder/EtaMatched", histograms[2]);
+    inTfile->GetObject("/jetFinder/Eta", histograms[3]);
+    inTfile->GetObject("/jetFinder/PhiMatched", histograms[4]);
+    inTfile->GetObject("/jetFinder/Phi", histograms[5]);
+    for (int i = 0; i < numberOfHists; ++i)
+    {
+        if (!histograms[i])
+        {
+            std::cout << "Histogram " << i << " failed to load!" << std::endl;
+            return 1;
+        }
+        else
+        {
+            histograms[i]->Sumw2();
+        }
+    }
+
+    TGraphAsymmErrors* etEffPlot = new TGraphAsymmErrors(histograms[0], histograms[1], "-b");
+    TGraphAsymmErrors* etaEffPlot = new TGraphAsymmErrors(histograms[2], histograms[3], "-b");
+    TGraphAsymmErrors* phiEffPlot = new TGraphAsymmErrors(histograms[4], histograms[5], "-b");
+    
+
+    TFile f(outFile.c_str(), "recreate");
+    etEffPlot->Write();
+    etaEffPlot->Write();
+    phiEffPlot->Write();
+    f.Close();
+
+
+
+
+
+
+
+}
