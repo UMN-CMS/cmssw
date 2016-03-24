fc54cc66618b80a5d71dd435147e9e816ca1190d
Andrew Evans <aevans@physics.umn.edu>
2016-03-24 08:52:43 -0500

    Quick patch checking that the format version is 1 in the amc payload before it is unpacked

--- EventFilter/HcalRawToDigi/src/HcalUnpacker.cc
+++ EventFilter/HcalRawToDigi/src/HcalUnpacker.cc
@@ -588,6 +588,7 @@ void HcalUnpacker::unpackUTCA(const FEDRawData& raw, const HcalElectronicsMap& e
     int nps=(amc13->AMCId(iamc)>>12)&0xF;
     
     HcalUHTRData uhtr(amc13->AMCPayload(iamc),amc13->AMCSize(iamc));
+    if(uhtr.getFormatVersion() != 1) continue;
 #ifdef DebugLog
     //debug printouts
     int nwords=uhtr.getRawLengthBytes()/2;
